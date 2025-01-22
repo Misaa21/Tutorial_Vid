9ea1de86d3aee4a79db8b290a0d6a56ad4e31eb8
Misaa21 <150657986+Misaa21@users.noreply.github.com>
2025-01-22 21:19:37 +0800

    Props

--- React_Tutorial/src/App.jsx
+++ React_Tutorial/src/App.jsx
@@ -1,5 +1,17 @@
-function App() {
+import Student from "./Student";
 
+
+function App() {
+  return(
+      <>
+        <Student name="Jiann" age = {21} isStudent={true}/>
+        <Student name="Yuri" age = {21} isStudent={false}/>
+        <Student name="Ethan" age = {22} isStudent={true}/>
+        <Student name="Croi" age = {16} isStudent={false}/>
+        <Student/>
+
+      </>
+  );
 }
 
 export default App
